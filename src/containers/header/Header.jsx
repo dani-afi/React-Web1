b0a060025a163944778b3f16fecf6383324f3b94
Daniel <daniel.afilipoaie@gmail.com>
2022-03-08 17:32:04 +0200

    deploy

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,19 +1,13 @@
-import React from "react";
-import people from "../../assets/people.png";
-import ai from "../../assets/ai.png";
-import "./header.css";
+import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
+import './header.css';
 
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">
-        Let&apos;s Build Something amazing with GPT-3 OpenAI
-      </h1>
-      <p>
-        Yet bed any for travelling assistance indulgence unpleasing. Not
-        thoughts all exercise blessing. Indulgence way everything joy alteration
-        boisterous the attachment. Party we years to order allow asked of.
-      </p>
+      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
@@ -21,13 +15,13 @@ const Header = () => (
       </div>
 
       <div className="gpt3__header-content__people">
-        <img src={people} alt="people" />
+        <img src={people} />
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
 
     <div className="gpt3__header-image">
-      <img src={ai} alt="ai" />
+      <img src={ai} />
     </div>
   </div>
 );
